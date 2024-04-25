db4f1a96d33995c66d6547a9a7a580284109db83
João Augusto <jaugustovpires@gmail.com>
2024-04-25 11:37:56 -0300

    feat: Spring Boot initial commit

---
+++ Tasks-Management/src/main/java/br/com/project/TasksManagement/TasksManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.TasksManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksManagementApplication.class, args);
+	}
+
+}
