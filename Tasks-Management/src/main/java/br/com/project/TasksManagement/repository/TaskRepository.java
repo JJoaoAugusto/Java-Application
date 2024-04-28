ae9dbfd60236a95b941732e42ebc672b23100a42
João Augusto <jaugustovpires@gmail.com>
2024-04-28 16:58:03 -0300

    feat: UserRepository, TaskRepository

--- Tasks-Management/src/main/java/br/com/project/TasksManagement/repository/TaskRepository.java
+++ Tasks-Management/src/main/java/br/com/project/TasksManagement/repository/TaskRepository.java
@@ -1,5 +1,9 @@
 package br.com.project.TasksManagement.repository;
 
-public class TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.project.TasksManagement.model.TaskModel;
+
+public interface TaskRepository extends JpaRepository<TaskModel, Long> {
     
 }
--- Tasks-Management/src/main/java/br/com/project/TasksManagement/repository/UserRepository.java
+++ Tasks-Management/src/main/java/br/com/project/TasksManagement/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package br.com.project.TasksManagement.repository;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.project.TasksManagement.model.UserModel;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
 }
