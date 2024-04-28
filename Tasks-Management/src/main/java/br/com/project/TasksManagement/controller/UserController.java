fb741c297f6e574b321658160fbcaf7ce3752a12
João Augusto <jaugustovpires@gmail.com>
2024-04-28 14:28:58 -0300

    fix: project architecture

---
+++ Tasks-Management/src/main/java/br/com/project/TasksManagement/controller/UserController.java
@@ -0,0 +1,5 @@
+package br.com.project.TasksManagement.controller;
+
+public class UserController {
+    
+}
---
+++ Tasks-Management/src/main/java/br/com/project/TasksManagement/service/UserService.java
@@ -0,0 +1,5 @@
+package br.com.project.TasksManagement.service;
+
+public class UserService {
+    
+}
